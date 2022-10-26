9b89a9c9b7ff0904df769c2b6480d801bedb9143
KamilShape <kamilshape@gmail.com>
2022-10-26 17:10:09 +0200

    add inputs and array with countries

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-// const options = {
-//     method: 'GET',
-//     headers: {
-//         'X-RapidAPI-Key': 'b534c0e408msh8635e573fab0c3bp101a7cjsn2335a157fc33',
-//         'X-RapidAPI-Host': 'exerciseapi3.p.rapidapi.com'
-//     }
-// };
+import { countryList } from './countries.js'
 
-fetch('http://universities.hipolabs.com/search?name=middle&country=turkey')
+fetch('http://universities.hipolabs.com/search?name=&country=')
     .then(response => response.json())
     .then(response => console.log(response))
     .catch(err => console.error(err));
+
+console.log(countryList)
