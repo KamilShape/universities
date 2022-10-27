b5a027330c189f909fc139257e70efbff2dcfd0b
KamilShape <kamilshape@gmail.com>
2022-10-27 20:16:18 +0200

    adding handle input function

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
 import { countryList } from './countries.js'
 
-fetch('http://universities.hipolabs.com/search?name=&country=')
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+const nameInput = document.querySelector('.inputs_input--name')
+const searchButton = document.querySelector('.searchButton')
+const countryInput = document.querySelector('.inputs_input--country')
 
-console.log(countryList)
+let name = ''
+let country = ''
+
+
+
+const countries = function() {
+    console.log(countryInput.innerHTML)
+    countryList.forEach(country => {
+        let newCountry = document.createElement('option')
+        newCountry.innerText = country
+        newCountry.value = country
+        countryInput.appendChild(newCountry)
+    })
+}
+
+countries()
+const handleInput = function(e) {
+    console.log(e.target.value)
+}
+
+const showUniversities = function() {
+    if (name !== '' || country !== '') {
+        fetch(`http://universities.hipolabs.com/search?name=${name}&country=${country}`)
+            .then(response => response.json())
+            .then(response => console.log(response))
+            .catch(err => console.error(err));
+    } else {
+        console.log('Please fill name or country.')
+    }
+    name = ''
+    country = ''
+}
+
+nameInput.addEventListener('input', handleInput)
+countryInput.addEventListener('input', handleInput)
+searchButton.addEventListener('click', showUniversities)
